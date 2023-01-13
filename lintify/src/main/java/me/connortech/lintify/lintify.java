4f12a825ce293a9cd59fbfdc5665e6db49b13745
Connor Koehler <connortech97@gmail.com>
2023-01-12 19:56:40 -0500

    Basic Project Creation

---
+++ lintify/src/main/java/me/connortech/lintify/lintify.java
@@ -0,0 +1,12 @@
+package me.connortech.lintify;
+
+import picocli.CommandLine;
+
+@CommandLine.Command(name = "Lintify", version = "BETA", mixinStandardHelpOptions = true)
+public class lintify implements Runnable {
+
+    @Override
+    public void run() {
+
+    }
+}
